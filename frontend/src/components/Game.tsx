bc3e4c51764b1749374bf75b3c13eaa0cd595904
Labbey Ammar Ahmed <141624572+ammar-codeable@users.noreply.github.com>
2024-08-13 14:58:27 +0530

    implement game logic

--- frontend/src/components/Game.tsx
+++ frontend/src/components/Game.tsx
@@ -1,46 +1,32 @@
 import { useState } from "react";
-import Board from "./Board";
-import { Button } from "./ui/button";
+import MainBoard from "./Main-Board";
+import MoveNavigation from "./Move-Navigation";
 
 export default function Game() {
-	const [moveHistory, setMoveHistory] = useState([Array(9).fill(null)]);
+	const [boardState, setBoardState] = useState(
+		Array(9).fill(Array(9).fill(null)),
+	);
 	const [currentMove, setCurrentMove] = useState(0);
 	const currentPlayer = currentMove % 2 === 0 ? "X" : "O";
-	const currentSquares = moveHistory[currentMove];
 
-	function handlePlay(nextSquares) {
-		setMoveHistory([...moveHistory.slice(0, currentMove + 1), nextSquares]);
-		setCurrentMove(currentMove + 1);
+	function handlePlay(nextSquares, boardId) {
+		setBoardState((boardState) => {
+			boardState[boardId] = nextSquares;
+			return boardState;
+		});
+		setCurrentMove((currentMove) => currentMove + 1);
 	}
-
-	const moves = moveHistory.map((_, move) => {
-		let moveDescription;
-		if (move > 0) {
-			moveDescription = `Go to move ${move}`;
-		} else {
-			moveDescription = "Go to game start";
-		}
-		return (
-			<li key={move}>
-				<Button className="m-1" onClick={() => setCurrentMove(move)}>
-					{moveDescription}
-				</Button>
-			</li>
-		);
-	});
 
 	return (
 		<div className="flex h-screen flex-col place-content-center place-items-center">
 			<div className="m-4">
-				<Board
+				<MainBoard
 					playerTurn={currentPlayer}
-					boardState={currentSquares}
+					mainBoardState={boardState}
 					onPlay={handlePlay}
 				/>
 			</div>
-			<div>
-				<ol className="flex flex-wrap">{moves}</ol>
-			</div>
+			<MoveNavigation />
 		</div>
 	);
 }
--- frontend/src/components/Square.tsx
+++ frontend/src/components/Square.tsx
@@ -1,8 +1,14 @@
 import { Button } from "./ui/button";
 
-function Square({ value, onSquareClick }) {
+function Square({ value, onSquareClick, cellId, boardId }) {
 	return (
-		<Button variant="outline" className="size-16 text-5xl" onClick={onSquareClick}>
+		<Button
+			variant="outline"
+			className="size-16 text-5xl"
+			onClick={() => {
+				onSquareClick(cellId, boardId);
+			}}
+		>
 			{value}
 		</Button>
 	);
