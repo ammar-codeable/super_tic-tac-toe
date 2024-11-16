697f85fffab6bbffc757612af52ce340d895639b
Labbey Ammar Ahmed <141624572+ammar-codeable@users.noreply.github.com>
2024-11-16 18:55:50 +0530

    feat: add preview configuration with port setting

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,10 +3,13 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": "/src",
-    }
-  }
+    },
+  },
+  preview: {
+    port: 80,
+  },
 });
