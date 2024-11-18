85b450972137da67f68c9e5b92381d7972cb6433
Labbey Ammar Ahmed <141624572+ammar-codeable@users.noreply.github.com>
2024-11-18 12:49:51 +0530

    feat: add redirect from root to play route in router

--- apps/web/src/app/router.tsx
+++ apps/web/src/app/router.tsx
@@ -2,6 +2,7 @@ import ChooseGameMode from "@/app/routes/choose-game-mode";
 import OfflineGame from "@/app/routes/offline-game";
 import OnlineGame from "@/app/routes/online-game";
 import Root from "@/app/routes/root";
+import { Navigate } from "react-router-dom";
 import {
   createBrowserRouter,
   createRoutesFromElements,
@@ -11,6 +12,7 @@ import {
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Root />}>
+      <Route index element={<Navigate to="/play" />} />
       <Route path="/play" element={<ChooseGameMode />} />
       <Route path="/play/offline" element={<OfflineGame />} />
       <Route path="/play/online" element={<OnlineGame />} />
