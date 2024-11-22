ab5fb6785878077a1f8dad898e60f50db93d3787
Labbey Ammar Ahmed <141624572+ammar-codeable@users.noreply.github.com>
2024-11-22 14:29:40 +0530

    fix: configure vite config correctly to use https on preview

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,35 +2,23 @@ import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  if (command === "build") {
-    return {
-      plugins: [react()],
-      resolve: {
-        alias: {
-          "@": "/src",
-        },
+export default defineConfig(() => {
+  const commonConfig = {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": "/src",
       },
-      preview: {
-        port: 443,
+    },
+    preview: {
+      host: "0.0.0.0",
+      port: 443,
+      https: {
+        key: process.env.VITE_SSL_KEY_PATH,
+        cert: process.env.VITE_SSL_CERT_PATH,
       },
-      server: {
-        https: {
-          key: "import.meta.env.VITE_SSL_KEY_PATH",
-          cert: "import.meta.env.VITE_SSL_CERT_PATH",
-        },
-        host: "0/.0.0.0",
-        port: 443,
-      },
-    };
-  } else {
-    return {
-      plugins: [react()],
-      resolve: {
-        alias: {
-          "@": "/src",
-        },
-      },
-    };
-  }
+    },
+  };
+
+  return commonConfig;
 });
