47da6aeae6a9923140e826330ca7f69ce65e513a
Labbey Ammar Ahmed <141624572+ammar-codeable@users.noreply.github.com>
2024-11-26 15:54:42 +0530

    refactor: remove unused resetGame utility function

--- packages/utils/src/reset-game.ts
+++
@@ -1,9 +0,0 @@
-function resetGame(
-	setMoveHistory: (moveHistory: number[][]) => void,
-	setCurrentMove: (currentMove: number) => void,
-) {
-	setMoveHistory([[-1, -1]]);
-	setCurrentMove(0);
-}
-
-export default resetGame;
