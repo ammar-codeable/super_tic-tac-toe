6c17f6e4168b8f394b70e491dab83eec182e469c
Labbey Ammar Ahmed <141624572+ammar-codeable@users.noreply.github.com>
2024-12-06 23:51:04 +0530

    refactor: implement sendSocketMessage utility for streamlined message sending to clients

--- apps/socket/src/index.ts
+++ apps/socket/src/index.ts
@@ -22,6 +22,7 @@ import {
 } from "./managers/game-manager";
 import { ClientMessageSchema } from "./schemas/socket-schemas";
 import { assignMark } from "./utils/assign-mark";
+import { sendSocketMessage } from "./utils/socket-utils";
 
 config();
 
@@ -62,7 +63,7 @@ const pendingPlayers: ws[] = [];
 
 wss.on("connection", (ws) => {
 	pendingPlayers.push(ws);
-	ws.send(JSON.stringify({ type: "waiting", waiting: true }));
+	sendSocketMessage([ws], { type: "waiting", waiting: true });
 
 	if (pendingPlayers.length >= 2) {
 		const player1Socket = pendingPlayers.shift()!;
@@ -71,22 +72,18 @@ wss.on("connection", (ws) => {
 		const [gameId, game] = createNewGame(player1Socket, player2Socket);
 		addGame(gameId, game);
 
-		player1Socket.send(JSON.stringify({ type: "init", gameId }));
-		player2Socket.send(JSON.stringify({ type: "init", gameId }));
-		player1Socket.send(JSON.stringify({ type: "waiting", waiting: false }));
-		player2Socket.send(JSON.stringify({ type: "waiting", waiting: false }));
+		sendSocketMessage([player1Socket, player2Socket], { type: "init", gameId });
+		sendSocketMessage([player1Socket, player2Socket], { type: "waiting", waiting: false });
 	}
 
 	ws.on("message", (data) => {
 		const message = validateMessage(data.toString(), ClientMessageSchema);
 		const game = getGame(message.gameId);
 		if (!game) {
-			ws.send(
-				JSON.stringify({
-					type: "error",
-					error: "Game not found",
-				})
-			);
+			sendSocketMessage([ws], {
+				type: "error",
+				error: "Game not found",
+			});
 			return;
 		}
 
@@ -94,93 +91,68 @@ wss.on("connection", (ws) => {
 		const opponent = getOpponent(message.gameId, ws);
 
 		if (!currentPlayer || !opponent) {
-			ws.send(
-				JSON.stringify({
-					type: "error",
-					error: "Player not found in game",
-				})
-			);
+			sendSocketMessage([ws], {
+				type: "error",
+				error: "Player not found in game",
+			});
 			return;
 		}
 
 		switch (message.type) {
 			case "player":
 				if (!game.players.player1 || !game.players.player2) {
-					currentPlayer.socket.send(
-						JSON.stringify({
-							type: "error",
-							error: "Both players must be connected before selecting a mark.",
-						})
-					);
+					sendSocketMessage([currentPlayer.socket], {
+						type: "error",
+						error: "Both players must be connected before selecting a mark.",
+					});
 					return;
 				}
 
 				assignMark(currentPlayer, opponent, message.player);
 
 				if (currentPlayer.mark && opponent.mark) {
-					currentPlayer.socket.send(
-						JSON.stringify({ type: "mark", mark: currentPlayer.mark })
-					);
-					opponent.socket.send(
-						JSON.stringify({ type: "mark", mark: opponent.mark })
-					);
+					sendSocketMessage([currentPlayer.socket], { type: "mark", mark: currentPlayer.mark });
+					sendSocketMessage([opponent.socket], { type: "mark", mark: opponent.mark });
 				}
 				break;
 
 			case "move":
 				const result = updateGameState(message.gameId, ...message.move);
 
-				opponent.socket!.send(
-					JSON.stringify({
-						type: "game",
-						game,
-					})
-				);
+				sendSocketMessage([opponent.socket], {
+					type: "game",
+					game,
+				});
 
 				if (result) {
-					currentPlayer.socket!.send(
-						JSON.stringify({ type: "result", result })
-					);
-					opponent.socket!.send(JSON.stringify({ type: "result", result }));
+					sendSocketMessage([currentPlayer.socket, opponent.socket], { type: "result", result });
 				}
 				break;
 
 			case "resign":
 				const resignResult = handleResign(message.gameId, ws);
-
-				currentPlayer.socket!.send(
-					JSON.stringify({ type: "result", result: resignResult })
-				);
-				opponent.socket!.send(
-					JSON.stringify({ type: "result", result: resignResult })
-				);
+				sendSocketMessage([currentPlayer.socket, opponent.socket], { type: "result", result: resignResult });
 				break;
 
 			case "chat":
-				const senderRole =
-					currentPlayer === game.players.player1 ? "player1" : "player2";
+				const senderRole = currentPlayer === game.players.player1 ? "player1" : "player2";
 				const chatMessage: ChatMessage = {
 					text: message.chat.text,
 					sender: senderRole,
 				};
 				addMessage(message.gameId, chatMessage);
-				opponent.socket.send(
-					JSON.stringify({ type: "chat", chat: [chatMessage] })
-				);
+				sendSocketMessage([opponent.socket], { type: "chat", chat: [chatMessage] });
 				break;
 
 			case "draw-offer":
 				switch (message.action) {
 					case "offer":
-						opponent.socket.send(JSON.stringify({ type: "draw-offer" }));
+						sendSocketMessage([opponent.socket], { type: "draw-offer" });
 						break;
 
 					case "accept":
 						const result = "DRAW";
-						currentPlayer.socket.send(
-							JSON.stringify({ type: "result", result })
-						);
-						opponent.socket.send(JSON.stringify({ type: "result", result }));
+						sendSocketMessage([currentPlayer.socket, opponent.socket], { type: "result", result });
 						game.result = result;
 						break;
 				}
@@ -188,32 +160,27 @@ wss.on("connection", (ws) => {
 
 			case "rematch":
 				if (opponent.socket.readyState !== ws.OPEN) {
-					currentPlayer.socket.send(
-						JSON.stringify({
-							type: "error",
-							error: "Opponent has disconnected",
-						})
-					);
+					sendSocketMessage([currentPlayer.socket], {
+						type: "error",
+						error: "Opponent has disconnected",
+					});
 					break;
 				}
 
 				switch (message.action) {
 					case "request":
-						opponent.socket.send(JSON.stringify({ type: "rematch-request" }));
+						sendSocketMessage([opponent.socket], { type: "rematch-request" });
 						break;
 
 					case "accept": {
 						resetGame(message.gameId);
 						swapPlayerMarks(message.gameId);
-
-						[currentPlayer, opponent].forEach((player) => {
-							player.socket.send(JSON.stringify({ type: "rematch-accepted" }));
-						});
+						sendSocketMessage([currentPlayer.socket, opponent.socket], { type: "rematch-accepted" });
 						break;
 					}
 
 					case "decline":
-						opponent.socket.send(JSON.stringify({ type: "rematch-declined" }));
+						sendSocketMessage([opponent.socket], { type: "rematch-declined" });
 						break;
 				}
 				break;
