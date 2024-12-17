9dfefac4652983e799f5ecfa0130bb83498e98db
Labbey Ammar Ahmed <141624572+ammar-codeable@users.noreply.github.com>
2024-12-18 02:27:03 +0530

    build: remove SSL options for production in Vite configuration
    Moving deploying from Digital Ocean droplet.
    SSL configuration is no longer required as cloudflare pages will handle the TLS encryption.

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,47 +1,24 @@
 import react from "@vitejs/plugin-react-swc";
-import { config } from "dotenv";
 import path from "path";
 import { defineConfig } from "vite";
 
-config();
-
-export default defineConfig(() => {
-  if (process.env.NODE_ENV === "production") {
-    if (!process.env.VITE_SSL_CERT_PATH || !process.env.VITE_SSL_KEY_PATH) {
-      throw new Error(
-        "SSL certificate paths not configured in environment variables",
-      );
-    }
-  }
-
-  const commonConfig = {
-    plugins: [react()],
-    resolve: {
-      alias: {
-        "@": "/src",
-        "@super-tic-tac-toe/constants": path.resolve(
-          __dirname,
-          "../../packages/constants/src",
-        ),
-        "@super-tic-tac-toe/utils": path.resolve(
-          __dirname,
-          "../../packages/utils/src",
-        ),
-        "@super-tic-tac-toe/types": path.resolve(
-          __dirname,
-          "../../packages/types/src",
-        ),
-      },
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": "/src",
+      "@super-tic-tac-toe/constants": path.resolve(
+        __dirname,
+        "../../packages/constants/src",
+      ),
+      "@super-tic-tac-toe/utils": path.resolve(
+        __dirname,
+        "../../packages/utils/src",
+      ),
+      "@super-tic-tac-toe/types": path.resolve(
+        __dirname,
+        "../../packages/types/src",
+      ),
     },
-    preview: {
-      host: "0.0.0.0",
-      port: 443,
-      https: {
-        key: process.env.VITE_SSL_KEY_PATH,
-        cert: process.env.VITE_SSL_CERT_PATH,
-      },
-    },
-  };
-
-  return commonConfig;
+  },
 });
