a0a9e7dafc8f12d197a8f14634aaa3cbae810cf4
Labbey Ammar Ahmed <141624572+ammar-codeable@users.noreply.github.com>
2025-01-03 20:23:32 +0530

    fix: explicitly define production environment setup for SSL configuration in socket server

--- apps/socket/src/index.ts
+++ apps/socket/src/index.ts
@@ -5,9 +5,9 @@ import { readFileSync } from "fs";
 import https from "https";
 import { WebSocketServer } from "ws";
 import {
-  handleDisconnect,
-  handleMessage,
-  handlePlayerConnection,
+	handleDisconnect,
+	handleMessage,
+	handlePlayerConnection,
 } from "./controllers/socket-controller";
 
 config();
@@ -20,35 +20,37 @@ app.use(express.json());
 const port = process.env.PORT;
 
 let server;
-if (process.env.NODE_ENV === "production") {
-  const certPath = process.env.SSL_CERT_PATH;
-  const keyPath = process.env.SSL_KEY_PATH;
-
-  if (!certPath || !keyPath) {
-    throw new Error(
-      "SSL certificate paths not configured in environment variables",
-    );
-  }
-
-  const httpsOptions = {
-    cert: readFileSync(certPath),
-    key: readFileSync(keyPath),
-  };
-
-  server = https.createServer(httpsOptions, app).listen(port);
-} else {
+if (process.env.NODE_ENV === "development") {
   server = app.listen(port);
+}
+
+if (process.env.NODE_ENV === "production") {
+	const certPath = process.env.SSL_CERT_PATH;
+	const keyPath = process.env.SSL_KEY_PATH;
+
+	if (!certPath || !keyPath) {
+		throw new Error(
+			"SSL certificate paths not configured in environment variables"
+		);
+	}
+
+	const httpsOptions = {
+		cert: readFileSync(certPath),
+		key: readFileSync(keyPath),
+	};
+
+	server = https.createServer(httpsOptions, app).listen(port);
 }
 
 const wss = new WebSocketServer({ server });
 
 app.get("/player-count", (_, res) => {
-  res.json({ count: wss.clients.size });
+	res.json({ count: wss.clients.size });
 });
 
 wss.on("connection", (ws) => {
-  handlePlayerConnection(ws);
+	handlePlayerConnection(ws);
 
-  ws.on("message", (data) => handleMessage(ws, data));
-  ws.on("close", () => handleDisconnect(ws));
+	ws.on("message", (data) => handleMessage(ws, data));
+	ws.on("close", () => handleDisconnect(ws));
 });
